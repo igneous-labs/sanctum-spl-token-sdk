6226487fafdb20c6dc3999b9196d9eb15e3fbd84
billythedummy <dhy1996@live.com.sg>
2025-04-28 15:04:03 +0800

    add IDs

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -3,3 +3,7 @@
 pub mod instructions;
 pub mod state;
 pub mod typedefs;
+
+pub const ID_STR: &str = "TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA";
+
+pub const ID: [u8; 32] = const_crypto::bs58::decode_pubkey(ID_STR);
